a5f4d230e3a256efffd814428f24b977259bce26
LuanHenrique0 <xyzzz1346@gmail.com>
2026-04-14 10:04:37 -0300

    Update ex11.js

--- ex11.js
+++ ex11.js
@@ -3,9 +3,9 @@ const prompt = require('readline-sync')
 
 let tamanhoTotal = 20
 
-let quantidadeBaixada = prompt.questionInt('Quantos gigas foram baixados?: ')
+let quantidadeBaixada = prompt.questionInt('Quantos GB foram baixados?: ')
 let percentual = (quantidadeBaixada / tamanhoTotal) * 100
 
-console.log('\n========== Progresso do Download ==========' )
+console.log('========== Progresso do Download ==========' )
 console.log(`Você já baixou ${Math.round(percentual)}% do arquivo.`)
-console.log('========================================\n')
+console.log('========================================')
