e0f91b026e674be6e656c70d7b2808453ad1a9ec
LuanHenrique0 <xyzzz1346@gmail.com>
2026-04-14 09:30:09 -0300

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -3,8 +3,8 @@ const readline = require('readline-sync');
 
 let nome = readline.question("Digite seu nome: ")
 
-if (nome == "Alice") {
-console.log("Olá, Alice! Bem-vinda de volta!")
+if (nome == "Luan") {
+console.log("Olá, Luan! Bem-vindo de volta!")
 } else {
 console.log(`Olá, ${nome}! Prazer em conhecê-lo!`)
  }
